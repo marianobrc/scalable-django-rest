561727f03b2785e7a99f153ba9b5b72e2bd5535c
marianobrc <marianomartinez84@gmail.com>
2022-08-28 13:42:21 -0300

    Add a users app with a custom user model (email auth)

---
+++ src/users/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager using email instead of username.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email is mandatory'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None  # Remove the username field
+    email = models.EmailField(_('email address'), unique=True)  # Email is the new unique user identifier
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
