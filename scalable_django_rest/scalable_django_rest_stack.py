3eb34fac80e9584426c05fa98917f70a68bb5490
marianobrc <marianomartinez84@gmail.com>
2022-08-29 14:57:21 -0300

    Format cdk files with black

--- scalable_django_rest/scalable_django_rest_stack.py
+++ scalable_django_rest/scalable_django_rest_stack.py
@@ -5,8 +5,8 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class ScalableDjangoRestStack(Stack):
 
+class ScalableDjangoRestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
--- tests/unit/test_scalable_django_rest_stack.py
+++ tests/unit/test_scalable_django_rest_stack.py
@@ -10,6 +10,7 @@ def test_sqs_queue_created():
     stack = ScalableDjangoRestStack(app, "scalable-django-rest")
     template = assertions.Template.from_stack(stack)
 
+
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
