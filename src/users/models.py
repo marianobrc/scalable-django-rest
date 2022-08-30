4be236d6c537eb1f91266b79bc143f4ae009f66b
marianobrc <marianomartinez84@gmail.com>
2022-08-30 19:48:21 -0300

    Convert emails to lowercase on user creation

--- src/users/models.py
+++ src/users/models.py
@@ -16,7 +16,7 @@ class CustomUserManager(BaseUserManager):
         """
         if not email:
             raise ValueError(_("The Email is mandatory"))
-        email = self.normalize_email(email)
+        email = self.normalize_email(email.lower())
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
