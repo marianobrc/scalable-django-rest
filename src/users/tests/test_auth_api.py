7077b0221927042495ec32da34bde2779a28e920
marianobrc <marianomartinez84@gmail.com>
2022-09-01 21:05:36 -0300

    Add a test for JWT tokens verification

--- src/users/tests/test_auth_api.py
+++ src/users/tests/test_auth_api.py
@@ -38,3 +38,24 @@ def test_logout(create_user, api_client):
     logout_url = reverse("rest_logout")
     response = api_client.post(logout_url, format="json")
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_jwt_verification_with_valid_token(create_user, api_client):
+    test_email = "user@test.com"
+    test_password = "MyPassw0Rd123"
+    create_user(email=test_email, password=test_password)
+    # Login to get a valid token
+    login_data = {
+        "username": test_email,
+        "password": test_password,
+    }
+    login_url = reverse("rest_login")
+    response = api_client.post(login_url, data=login_data, format="json")
+    response_data = response.json()
+    access_token = response_data["access_token"]
+    assert response.status_code == status.HTTP_200_OK
+    # Call the verification endpoint with a valid token
+    token_verification_url = reverse("token_verify")
+    response = api_client.post(token_verification_url, data={"token":access_token}, format="json")
+    assert response.status_code == status.HTTP_200_OK
