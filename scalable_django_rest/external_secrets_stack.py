960e2808caf2d17f814e95c61faa1a4e642248bb
marianobrc <marianomartinez84@gmail.com>
2022-09-16 17:38:34 -0300

    Add a cdk stack for secrets stored in secrets manager

---
+++ scalable_django_rest/external_secrets_stack.py
@@ -0,0 +1,55 @@
+from aws_cdk import (
+    Stack,
+    aws_ecs as ecs,
+    aws_secretsmanager as secretsmanager,
+)
+from constructs import Construct
+
+
+class ExternalSecretsStack(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        database_secrets: secretsmanager.ISecret,
+        name_prefix: str,  # Naming convention for parameters: i.e; /AppNameStageName/SecretName
+        **kwargs,
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Secret values required by the app which are store in the Secrets Manager
+        # This values will be injected as env vars on runtime
+        self.app_secrets = {
+            "DJANGO_SECRET_KEY": ecs.Secret.from_secrets_manager(
+                secretsmanager.Secret.from_secret_name_v2(
+                    self,
+                    f"DjangoKeySecret",
+                    secret_name=f"{name_prefix}DjangoSecretKey",
+                )
+            ),
+            "DB_HOST": ecs.Secret.from_secrets_manager(database_secrets, field="host"),
+            "DB_PORT": ecs.Secret.from_secrets_manager(database_secrets, field="port"),
+            "DB_NAME": ecs.Secret.from_secrets_manager(
+                database_secrets, field="dbname"
+            ),
+            "DB_USER": ecs.Secret.from_secrets_manager(
+                database_secrets, field="username"
+            ),
+            "DB_PASSWORD": ecs.Secret.from_secrets_manager(
+                database_secrets, field="password"
+            ),
+            "AWS_ACCESS_KEY_ID": ecs.Secret.from_secrets_manager(
+                secretsmanager.Secret.from_secret_name_v2(
+                    self,
+                    f"AWSAccessKeyIDSecret",
+                    secret_name=f"{name_prefix}AwsApiKeyId",
+                )
+            ),
+            "AWS_SECRET_ACCESS_KEY": ecs.Secret.from_secrets_manager(
+                secretsmanager.Secret.from_secret_name_v2(
+                    self,
+                    f"AWSAccessKeySecretSecret",
+                    secret_name=f"{name_prefix}AwsApiKeySecret",
+                )
+            ),
+        }
