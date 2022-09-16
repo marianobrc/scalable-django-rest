63ccb504cc008c45d177b0a23c33a394e2866372
marianobrc <marianomartinez84@gmail.com>
2022-09-16 17:52:04 -0300

    Add a stack for route53 adn dns records

--- scalable_django_rest/dns_route_to_alb_stack.py
+++ scalable_django_rest/dns_route_to_alb_stack.py
@@ -8,26 +8,23 @@ from constructs import Construct
 
 
 class DnsRouteToAlbStack(Stack):
-
     def __init__(
-            self,
-            scope: Construct,
-            construct_id: str,
-            alb: elbv2.ApplicationLoadBalancer,
-            domain_name: str,  # example.com
-            subdomain: str = None,  # api(.example.com)
-            **kwargs
+        self,
+        scope: Construct,
+        construct_id: str,
+        alb: elbv2.ApplicationLoadBalancer,
+        domain_name: str,  # example.com
+        subdomain: str = None,  # api(.example.com)
+        **kwargs
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
         self.hosted_zone = route53.HostedZone.from_lookup(
-            self,
-            "HostedZone",
-            domain_name=domain_name
+            self, "HostedZone", domain_name=domain_name
         )
         self.dns_record = route53.ARecord(
             self,
             "ARecord",
             zone=self.hosted_zone,
             record_name=subdomain,
-            target=route53.RecordTarget.from_alias(targets.LoadBalancerTarget(alb))
+            target=route53.RecordTarget.from_alias(targets.LoadBalancerTarget(alb)),
         )
