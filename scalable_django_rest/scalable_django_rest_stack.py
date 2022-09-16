a79f44a576b9f74e988d5ce5d07e6af9d7009b03
marianobrc <marianomartinez84@gmail.com>
2022-09-16 17:31:01 -0300

    Add the cdk stack for the django api

--- scalable_django_rest/scalable_django_rest_stack.py
+++ scalable_django_rest/scalable_django_rest_stack.py
@@ -1,19 +1,122 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_sqs as sqs,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_certificatemanager as acm,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_ssm as ssm,
 )
 from constructs import Construct
 
 
 class ScalableDjangoRestStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        vpc: ec2.Vpc,
+        ecs_cluster: ecs.Cluster,
+        queue: sqs.Queue,
+        env_vars: dict,
+        secrets: dict,
+        task_cpu: int = 256,
+        task_memory_mib: int = 1024,
+        task_desired_count: int = 2,
+        task_min_scaling_capacity: int = 2,
+        task_max_scaling_capacity: int = 4,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.vpc = vpc
+        self.ecs_cluster = ecs_cluster
+        self.queue = queue
+        self.env_vars = env_vars
+        self.secrets = secrets
+        self.task_cpu = task_cpu
+        self.task_memory_mib = task_memory_mib
+        self.task_desired_count = task_desired_count
+        self.task_min_scaling_capacity = task_min_scaling_capacity
+        self.task_max_scaling_capacity = task_max_scaling_capacity
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ScalableDjangoRestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Prepare parameters
+        self.container_name = f"django_app"
+        # Retrieve the arn of the TLS certificate from SSM Parameter Store
+        self.certificate_arn = ssm.StringParameter.value_for_string_parameter(
+            self, f"/{scope.stage_name}/CertificateArn"
+        )
+        # Instantiate the certificate which will be required by the load balancer later
+        self.domain_certificate = acm.Certificate.from_certificate_arn(
+            self, "DomainCertificate", certificate_arn=self.certificate_arn
+        )
+        # Create the load balancer, ECS service and fargate task for teh Django App
+        self.alb_fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            f"MyDjangoApp",
+            protocol=elbv2.ApplicationProtocol.HTTPS,
+            certificate=self.domain_certificate,
+            redirect_http=True,
+            platform_version=ecs.FargatePlatformVersion.VERSION1_4,
+            cluster=self.ecs_cluster,  # Required
+            task_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+            ),
+            cpu=self.task_cpu,  # Default is 256
+            memory_limit_mib=self.task_memory_mib,  # Default is 512
+            desired_count=self.task_desired_count,  # Default is 1
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_asset(
+                    directory="app/", file="docker/app/Dockerfile", target="prod"
+                ),
+                container_name=self.container_name,
+                container_port=8000,
+                environment=self.env_vars,
+                secrets=self.secrets,
+            ),
+            public_load_balancer=True,
+        )
+        # Set the health checks settings
+        self.alb_fargate_service.target_group.configure_health_check(
+            path="/status/", healthy_threshold_count=3, unhealthy_threshold_count=2
+        )
+        # Autoscaling based on CPU utilization
+        scalable_target = self.alb_fargate_service.service.auto_scale_task_count(
+            min_capacity=self.task_min_scaling_capacity,
+            max_capacity=self.task_max_scaling_capacity,
+        )
+        scalable_target.scale_on_cpu_utilization(
+            f"CpuScaling",
+            target_utilization_percent=75,
+        )
+        # Save useful values in in SSM
+        self.ecs_cluster_name_param = ssm.StringParameter(
+            self,
+            "EcsClusterNameParam",
+            parameter_name=f"/{scope.stage_name}/EcsClusterNameParam",
+            string_value=self.ecs_cluster.cluster_name,
+        )
+        self.task_def_arn_param = ssm.StringParameter(
+            self,
+            "TaskDefArnParam",
+            parameter_name=f"/{scope.stage_name}/TaskDefArnParam",
+            string_value=self.alb_fargate_service.task_definition.task_definition_arn,
+        )
+        self.task_def_family_param = ssm.StringParameter(
+            self,
+            "TaskDefFamilyParam",
+            parameter_name=f"/{scope.stage_name}/TaskDefFamilyParam",
+            string_value=f"family:{self.alb_fargate_service.task_definition.family}",
+        )
+        self.exec_role_arn_param = ssm.StringParameter(
+            self,
+            "TaskExecRoleArnParam",
+            parameter_name=f"/{scope.stage_name}/TaskExecRoleArnParam",
+            string_value=self.alb_fargate_service.task_definition.execution_role.role_arn,
+        )
+        self.task_role_arn_param = ssm.StringParameter(
+            self,
+            "TaskRoleArnParam",
+            parameter_name=f"/{scope.stage_name}/TaskRoleArnParam",
+            string_value=self.alb_fargate_service.task_definition.task_role.role_arn,
+        )
---
+++ scalable_django_rest/static_files_stack.py
@@ -0,0 +1,88 @@
+import typing
+from aws_cdk import (
+    Stack,
+    RemovalPolicy,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_ssm as ssm
+)
+from constructs import Construct
+
+
+class StaticFilesStack(Stack):
+
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            bucket_name: str = None,
+            cors_allowed_origins: typing.Sequence[str] = None,
+            **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.bucket_name = bucket_name
+        self.cors_allowed_origins = cors_allowed_origins
+        # Create a private bucket
+        self.s3_bucket = s3.Bucket(
+            self, f"Bucket",
+            bucket_name=bucket_name,  # Bucket name must be globally unique. If not set it's assigned by Cloudformation
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY,  # Delete objects on bucket removal
+            auto_delete_objects=True
+        )
+        # Add an OriginAccessIdentity to access the bucket
+        self.oai = cloudfront.OriginAccessIdentity(
+            self, f"BucketOAI",
+            comment="OAI to access backend static files."
+        )
+        self.s3_bucket.grant_read(self.oai)
+        # Prepare CORS settings
+        if self.cors_allowed_origins:
+            response_headers_policy = cloudfront.ResponseHeadersPolicy(
+                self, "ResponseHeadersPolicy",
+                response_headers_policy_name=f"{scope.stage_name}CORSPolicy",
+                comment="CORS Policy",
+                cors_behavior=cloudfront.ResponseHeadersCorsBehavior(
+                    access_control_allow_credentials=True,
+                    access_control_allow_headers=[
+                        "accept",
+                        "accept-encoding",
+                        "authorization",
+                        "content-type",
+                        "dnt",
+                        "origin",
+                        "user-agent",
+                        "x-csrftoken"
+                    ],
+                    access_control_allow_methods=["GET", "HEAD", "OPTIONS"],
+                    access_control_allow_origins=cors_allowed_origins,
+                    origin_override=True
+                )
+            )
+        else:
+            response_headers_policy = cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS
+        # Create the cloudfront distribution
+        self.cloudfront_distro = cloudfront.Distribution(
+            self, "CFDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(
+                    self.s3_bucket,
+                    origin_access_identity=self.oai
+                ),
+                response_headers_policy=response_headers_policy
+            )
+        )
+        # Save useful parameters to SSM Parameter Store
+        self.static_files_bucket_name = ssm.StringParameter(
+            self,
+            "StaticFilesBucketNameParam",
+            parameter_name=f"/{scope.stage_name}/StaticFilesBucketNameParam",
+            string_value=self.s3_bucket.bucket_name
+        )
+        self.static_files_cloudfront_url = ssm.StringParameter(
+            self,
+            "StaticFilesCloudFrontUrlParam",
+            parameter_name=f"/{scope.stage_name}/StaticFilesCloudFrontUrlParam",
+            string_value=self.cloudfront_distro.distribution_domain_name
+        )
