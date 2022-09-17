250a6550032681a4666c7488603431e6d57eb25a
marianobrc <marianomartinez84@gmail.com>
2022-09-17 12:20:23 -0300

    Add a cdk stack for SQS queues used by celery

--- scalable_django_rest/queues_stack.py
+++ scalable_django_rest/queues_stack.py
@@ -7,23 +7,14 @@ from constructs import Construct
 
 
 class QueuesStack(Stack):
-
-    def __init__(
-            self,
-            scope: Construct,
-            construct_id: str,
-            **kwargs
-    ) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         # Create a SQS queue
-        self.default_queue = sqs.Queue(
-            self,
-            "SQSQueue"
-        )
+        self.default_queue = sqs.Queue(self, "SQSQueue")
         # Save the queue url in SSM Parameter Store
         self.default_queue_url_param = ssm.StringParameter(
             self,
             "SqsDefaultQueueUrlParam",
             parameter_name=f"/{scope.stage_name}/SqsDefaultQueueUrlParam",
-            string_value=self.default_queue.queue_url
+            string_value=self.default_queue.queue_url,
         )
